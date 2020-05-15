8e9d576bcc9f5d962844c95bcff481de06dbea3b
KumarAbhishekShahi <abhishek.shahi@gmail.com>
2020-05-15 18:52:40 +0530

    Update main.tf

--- config/main.tf
+++ config/main.tf
@@ -9,15 +9,15 @@ provider "azurerm" {
 }
 
 # Create a new resource group
-resource "azurerm_resource_group" "rg" {
-    name     = "myTFResourceGroup"
-    location = "westus2"
+#resource "azurerm_resource_group" "rg" {
+#    name     = "myTFResourceGroup"
+#    location = "westus2"
     
-    tags = {
-        Environment = "Terraform Getting Started"
-        Team = "DevOps"   
-    }
-}
+#    tags = {
+#        Environment = "Terraform Getting Started"
+#        Team = "DevOps"   
+#    }
+#}
 
 # Create virtual network
 resource "azurerm_virtual_network" "vnet" {
