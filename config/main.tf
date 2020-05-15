30b93968c4c455515a33249146559a06b46c9ba7
KumarAbhishekShahi <abhishek.shahi@gmail.com>
2020-05-15 20:29:24 +0530

    Update main.tf

--- config/main.tf
+++ config/main.tf
@@ -1,36 +1,31 @@
 # Configure the Azure provider
 provider "azurerm" {
-  version = "~>1.31"
-  subscription_id = "c4b8ac58-e4e0-438d-a04e-30604e28751c"
-  client_id       = "0bb6def8-1b49-4703-bd15-9421dcc99b07"
-  client_secret   = "28e93f98-e3f1-46e4-ad86-f530715199be"
-  tenant_id       = "e97d547a-cdc9-48fa-a197-d2f73ab3e841"
-  features {}
+    version = "~>1.32.0"
 }
 
 # Create a new resource group
-#resource "azurerm_resource_group" "rg" {
-#    name     = "myTFResourceGroup"
-#    location = "westus2"
-    
-#    tags = {
-#        Environment = "Terraform Getting Started"
-#        Team = "DevOps"   
-#    }
-#}
+resource "azurerm_resource_group" "rg" {
+    name     = "myTFResourceGroup"
+    location = "westus2"
+
+    tags = {
+        Environment = "Terraform Getting Started"
+        Team = "DevOps"   
+    }
+}
 
 # Create virtual network
 resource "azurerm_virtual_network" "vnet" {
   name                = "myTFVnet"
   address_space       = ["10.0.0.0/16"]
   location            = "westus2"
-  resource_group_name = "myTFResourceGroup"
+  resource_group_name = azurerm_resource_group.rg.name
 }
 
 # Create subnet
 resource "azurerm_subnet" "subnet" {
   name                 = "myTFSubnet"
-  resource_group_name  =  "myTFResourceGroup"
+  resource_group_name  = azurerm_resource_group.rg.name
   virtual_network_name = azurerm_virtual_network.vnet.name
   address_prefix       = "10.0.1.0/24"
 }
@@ -39,7 +34,7 @@ resource "azurerm_subnet" "subnet" {
 resource "azurerm_public_ip" "publicip" {
   name                = "myTFPublicIP"
   location            = "westus2"
-  resource_group_name =  "myTFResourceGroup"
+  resource_group_name = azurerm_resource_group.rg.name
   allocation_method   = "Static"
 }
 
@@ -48,7 +43,7 @@ resource "azurerm_public_ip" "publicip" {
 resource "azurerm_network_security_group" "nsg" {
   name                = "myTFNSG"
   location            = "westus2"
-  resource_group_name =  "myTFResourceGroup"
+  resource_group_name = azurerm_resource_group.rg.name
 
   security_rule {
     name                       = "SSH"
@@ -67,7 +62,7 @@ resource "azurerm_network_security_group" "nsg" {
 resource "azurerm_network_interface" "nic" {
   name                      = "myNIC"
   location                  = "westus2"
-  resource_group_name       =  "myTFResourceGroup"
+  resource_group_name       = azurerm_resource_group.rg.name
   network_security_group_id = azurerm_network_security_group.nsg.id
 
   ip_configuration {
@@ -82,7 +77,7 @@ resource "azurerm_network_interface" "nic" {
 resource "azurerm_virtual_machine" "vm" {
   name                  = "myTFVM"
   location              = "westus2"
-  resource_group_name   =  "myTFResourceGroup"
+  resource_group_name   = azurerm_resource_group.rg.name
   network_interface_ids = [azurerm_network_interface.nic.id]
   vm_size               = "Standard_DS1_v2"
 
@@ -109,4 +104,4 @@ resource "azurerm_virtual_machine" "vm" {
   os_profile_linux_config {
     disable_password_authentication = false
   }
-}
+} 
